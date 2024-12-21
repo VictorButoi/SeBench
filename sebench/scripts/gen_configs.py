14094fc8db4347f1d7cb9017c3d7415e92c9f47d
Victor Ion Butoi <victor.butoi@gmail.com>
2024-12-21 16:29:12 -0500

    working through restructure so that it is project agnostic

---
+++ sebench/scripts/gen_configs.py
@@ -0,0 +1,264 @@
+# torch imports
+import torch
+# ionpy imports
+from datetime import datetime
+from ionpy.util.ioutil import autosave
+from ionpy.util.hash import json_digest
+from ionpy.analysis import ResultsLoader
+from ionpy.util import Config, dict_product
+from ionpy.experiment.util import absolute_import, generate_tuid
+from ionpy.util.config import check_missing, HDict, valmap, config_digest
+# misc imports
+import os
+import ast
+import json
+import yaml
+import inspect
+import itertools
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from pydantic import validate_arguments
+from typing import Any, Optional, Literal, List
+# local imports
+import sebench.scripts.utils as utils
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def get_training_configs(
+    exp_cfg: dict,
+    base_cfg: Config,
+    config_root: Path,
+    scratch_root: Path,
+    add_date: bool = True
+): 
+    # We need to flatten the experiment config to get the different options.
+    # Building new yamls under the exp_name name for model type.
+    exp_name = exp_cfg.pop('group')
+    train_exp_root = utils.get_exp_root(exp_name, group="training", add_date=add_date, scratch_root=scratch_root)
+
+    # Flatten the experiment config.
+    flat_exp_cfg_dict = utils.flatten_cfg2dict(exp_cfg)
+
+    # Add the dataset specific details.
+    train_dataset_name = flat_exp_cfg_dict['data._class'].split('.')[-1]
+    dataset_cfg_file = config_root /"training" / f"{train_dataset_name}.yaml"
+    if dataset_cfg_file.exists():
+        with open(dataset_cfg_file, 'r') as d_file:
+            dataset_train_cfg = yaml.safe_load(d_file)
+        # Update the base config with the dataset specific config.
+        base_cfg = base_cfg.update([dataset_train_cfg])
+    else:
+        print(f"Warning: No dataset specific train config found for {train_dataset_name}.")
+    
+    # Get the information about seeds.
+    seed = flat_exp_cfg_dict.pop('experiment.seed', 40)
+    seed_range = flat_exp_cfg_dict.pop('experiment.seed_range', 1)
+
+    # Create the ablation options.
+    option_set = {
+        'log.root': [str(train_exp_root)],
+        'experiment.seed': [seed + seed_idx for seed_idx in range(seed_range)],
+        **utils.listify_dict(flat_exp_cfg_dict)
+    }
+
+    # Get the configs
+    cfgs = utils.get_option_product(exp_name, option_set, base_cfg)
+
+    # Return the configs and the base config.
+    base_cfg_dict = base_cfg.to_dict()
+    # Finally, generate the uuid that identify each of the configs.
+    cfgs = utils.generate_config_uuids(cfgs)
+
+    return base_cfg_dict, cfgs
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def get_inference_configs(
+    exp_cfg: dict,
+    base_cfg: Config,
+    add_date: bool = True,
+    use_best_models: bool = False,
+    code_root: Path = Path("/storage/vbutoi/projects/SeBench"),
+    scratch_root: Path = Path("/storage/vbutoi/scratch/SeBench")
+):
+    # We need to flatten the experiment config to get the different options.
+    # Building new yamls under the exp_name name for model type.
+    # Save the experiment config.
+    group_str = exp_cfg.pop('group')
+    sub_group_str = exp_cfg.pop('subgroup', "")
+    exp_name = f"{group_str}/{sub_group_str}"
+
+    # Get the root for the inference experiments.
+    inference_log_root = utils.get_exp_root(exp_name, group="inference", add_date=add_date, scratch_root=scratch_root)
+
+    # In our general inference sheme, often we want to use the best models corresponding to a dataset
+    if add_date:
+        eval_dataset = group_str.split('_')[0]
+    else:
+        eval_dataset = group_str.split('_')[3] # Group format is like MM_DD_YY_Dataset
+    if eval_dataset in ['OCTA', 'ISLES', "WMH"] and use_best_models:
+        # Load the default best models, and update the exp config with those as the base models.
+        with open(code_root / "sebench" / "configs" / "defaults" / "Best_Models.yaml", 'r') as file:
+            best_models_cfg = yaml.safe_load(file)
+        # Update the exp_cfg with the best models.
+        exp_cfg['base_model'] = best_models_cfg[eval_dataset]
+    
+    # Flatten the config.
+    flat_exp_cfg_dict = utils.flatten_cfg2dict(exp_cfg)
+    # For any key that is a tuple we need to convert it to a list, this is an artifact of the flattening..
+    for key, val in flat_exp_cfg_dict.items():
+        if isinstance(val, tuple):
+            flat_exp_cfg_dict[key] = list(val)
+
+    # Sometimes we want to do a range of values to sweep over, we will know this by ... in it.
+    for key, val in flat_exp_cfg_dict.items():
+        if isinstance(val, list):
+            for idx, val_list_item in enumerate(val):
+                if isinstance(val_list_item, str) and '...' in val_list_item:
+                    # Replace the string with a range.
+                    flat_exp_cfg_dict[key][idx] = utils.get_range_from_str(val_list_item)
+        elif isinstance(val, str) and  '...' in val:
+            # Finally stick this back in as a string tuple version.
+            flat_exp_cfg_dict[key] = utils.get_range_from_str(val)
+
+    # Load the inference cfg from local.
+    ##################################################
+    default_cfg_root = code_root / "sebench" / "configs" / "defaults"
+    ##################################################
+    with open(default_cfg_root / "Calibration_Metrics.yaml", 'r') as file:
+        cal_metrics_cfg = yaml.safe_load(file)
+    ##################################################
+    base_cfg = base_cfg.update([cal_metrics_cfg])
+
+    # Gather the different config options.
+    cfg_opt_keys = list(flat_exp_cfg_dict.keys())
+    #First going through and making sure each option is a list and then using itertools.product.
+    for ico_key in flat_exp_cfg_dict:
+        if not isinstance(flat_exp_cfg_dict[ico_key], list):
+            flat_exp_cfg_dict[ico_key] = [flat_exp_cfg_dict[ico_key]]
+    
+    # Generate product tuples 
+    product_tuples = list(itertools.product(*[flat_exp_cfg_dict[key] for key in cfg_opt_keys]))
+
+    # Convert product tuples to dictionaries
+    total_run_cfg_options = [{cfg_opt_keys[i]: [item[i]] for i in range(len(cfg_opt_keys))} for item in product_tuples]
+
+    # Define the set of default config options.
+    default_config_options = {
+        'experiment.exp_name': [exp_name],
+        'experiment.exp_root': [str(inference_log_root)],
+    }
+    # Accumulate a set of config options for each dataset
+    dataset_cfgs = []
+    # Iterate through all of our inference options.
+    for run_opt_dict in total_run_cfg_options: 
+        # One required key is 'base_model'. We need to know if it is a single model or a group of models.
+        # We evaluate this by seeing if 'submitit' is in the base model path.
+        base_model_group_dir = Path(run_opt_dict.pop('base_model')[0])
+        if 'submitit' in os.listdir(base_model_group_dir):
+            model_set  = utils.gather_exp_paths(str(base_model_group_dir)) 
+        else:
+            model_set = [str(base_model_group_dir)]
+        # Append these to the list of configs and roots.
+        dataset_cfgs.append({
+            'log.root': [str(inference_log_root)],
+            'experiment.model_dir': model_set,
+            **run_opt_dict,
+            **default_config_options
+        })
+
+    # Keep a list of all the run configuration options.
+    cfgs = []
+    # Iterate over the different config options for this dataset. 
+    for option_dict in dataset_cfgs:
+        for exp_cfg_update in dict_product(option_dict):
+            # Add the inference dataset specific details.
+            dataset_inf_cfg_dict = get_inference_dset_info(
+                cfg=exp_cfg_update,
+                code_root=code_root
+            )
+            # Update the base config with the new options. Note the order is important here, such that 
+            # the exp_cfg_update is the last thing to update.
+            cfg = base_cfg.update([dataset_inf_cfg_dict, exp_cfg_update])
+            # Verify it's a valid config
+            check_missing(cfg)
+            # Add it to the total list of inference options.
+            cfgs.append(cfg)
+
+    # Return the configs and the base config.
+    base_cfg_dict = base_cfg.to_dict()
+    # Finally, generate the uuid that identify each of the configs.
+    cfgs = utils.generate_config_uuids(cfgs)
+
+    return base_cfg_dict, cfgs
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def get_restart_configs(
+    exp_cfg: dict,
+    base_cfg: Config,
+    add_date: bool = True,
+    scratch_root: Path = Path("/storage/vbutoi/scratch/SeBench"),
+    train_cfg_root: Path = Path("/storage/vbutoi/projects/SeBench/sebench/configs/training"),
+): 
+    # We need to flatten the experiment config to get the different options.
+    # Building new yamls under the exp_name name for model type.
+    exp_name = exp_cfg.pop('group')
+    restart_exp_root = utils.get_exp_root(exp_name, group="restarted", add_date=add_date, scratch_root=scratch_root)
+
+    # Get the flat version of the experiment config.
+    restart_cfg_dict = utils.flatten_cfg2dict(exp_cfg)
+
+    # If we are changing aspects of the dataset, we need to update the base config.
+    if 'data._class' in restart_cfg_dict:
+        # Add the dataset specific details.
+        dataset_cfg_file = train_cfg_root/ f"{restart_cfg_dict['data._class'].split('.')[-1]}.yaml"
+        if dataset_cfg_file.exists():
+            with open(dataset_cfg_file, 'r') as d_file:
+                dataset_train_cfg = yaml.safe_load(d_file)
+            # Update the base config with the dataset specific config.
+            base_cfg = base_cfg.update([dataset_train_cfg])
+        
+    # This is a required key. We want to get all of the models and vary everything else.
+    pretrained_dir_list = restart_cfg_dict.pop('train.pretrained_dir') 
+    if not isinstance(pretrained_dir_list, list):
+        pretrained_dir_list = [pretrained_dir_list]
+
+    # Now we need to go through all the pre-trained models and gather THEIR configs.
+    all_pre_models = []
+    for pre_model_dir in pretrained_dir_list:
+        if 'submitit' in os.listdir(pre_model_dir):
+            all_pre_models += utils.gather_exp_paths(pre_model_dir) 
+        else:
+            all_pre_models.append(pre_model_dir)
+
+    # Listify the dict for the product.
+    listy_pt_cfg_dict = {
+        'log.root': [str(restart_exp_root)],
+        **utils.listify_dict(restart_cfg_dict)
+    }
+    
+    # Go through all the pretrained models and add the new options for the restart.
+    cfgs = []
+    for pt_dir in all_pre_models:
+        # Load the pre-trained model config.
+        with open(f"{pt_dir}/config.yml", 'r') as file:
+            pt_exp_cfg = Config(yaml.safe_load(file))
+        # Make a copy of the listy_pt_cfg_dict.
+        pt_listy_cfg_dict = listy_pt_cfg_dict.copy()
+        pt_listy_cfg_dict['train.pretrained_dir'] = [pt_dir] # Put the pre-trained model back in.
+        # Update the pt_exp_cfg with the restart_cfg.
+        pt_restart_base_cfg = pt_exp_cfg.update([base_cfg])
+        pt_cfgs = utils.get_option_product(exp_name, pt_listy_cfg_dict, pt_restart_base_cfg)
+        # Append the list of configs for this pre-trained model.
+        cfgs += pt_cfgs
+
+    # Return the configs and the base config.
+    base_cfg_dict = base_cfg.to_dict()
+    # Finally, generate the uuid that identify each of the configs.
+    cfgs = utils.generate_config_uuids(cfgs)
+
+    return base_cfg_dict, cfgs
+
---
+++ sebench/scripts/utils.py
@@ -0,0 +1,201 @@
+# torch imports
+import torch
+# ionpy imports
+from datetime import datetime
+from ionpy.util.ioutil import autosave
+from ionpy.util.hash import json_digest
+from ionpy.analysis import ResultsLoader
+from ionpy.util import Config, dict_product
+from ionpy.experiment.util import absolute_import, generate_tuid
+from ionpy.util.config import check_missing, HDict, valmap, config_digest
+# misc imports
+import os
+import ast
+import json
+import yaml
+import inspect
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from pydantic import validate_arguments
+from typing import Any, Optional, Literal, List
+# local imports
+from ..metrics.utils import get_bin_per_sample
+
+
+def list2tuple(val):
+    if isinstance(val, list):
+        return tuple(map(list2tuple, val))
+    return val
+
+
+def get_exp_root(exp_name, group, add_date, scratch_root):
+    # Optionally, add today's date to the run name.
+    if add_date:
+        today_date = datetime.now()
+        formatted_date = today_date.strftime("%m_%d_%y")
+        exp_name = f"{formatted_date}_{exp_name}"
+    # Save the experiment config.
+    return scratch_root / group / exp_name
+
+
+def flatten_cfg2dict(cfg: Config):
+    cfg = HDict(cfg)
+    flat_exp_cfg = valmap(list2tuple, cfg.flatten())
+    return flat_exp_cfg
+
+
+def listify_dict(d):
+    listy_d = {}
+    # We need all of our options to be in lists as convention for the product.
+    for ico_key in d:
+        # If this is a tuple, then convert it to a list.
+        if isinstance(d[ico_key], tuple):
+            listy_d[ico_key] = list(d[ico_key])
+        # Otherwise, make sure it is a list.
+        elif not isinstance(d[ico_key], list):
+            listy_d[ico_key] = [d[ico_key]]
+        else:
+            listy_d[ico_key] = d[ico_key]
+    # Return the listified dictionary.
+    return listy_d
+
+
+def proc_cfg_name(
+    exp_name,
+    varying_keys,
+    cfg
+):
+    params = []
+    params.append("exp_name:" + exp_name)
+    for key, value in cfg.items():
+        if key in varying_keys:
+            if key not in ["log.root", "train.pretrained_dir"]:
+                key_name = key.split(".")[-1]
+                short_value = str(value).replace(" ", "")
+                if key_name == "exp_name":
+                    params.append(str(short_value))
+                else:
+                    params.append(f"{key_name}:{short_value}")
+    wandb_string = "-".join(params)
+    return {"log.wandb_string": wandb_string}
+
+
+def get_option_product(
+    exp_name,
+    option_set,
+    base_cfg
+):
+    # If option_set is not a list, make it a list
+    cfgs = []
+    # Get all of the keys that have length > 1 (will be turned into different options)
+    varying_keys = [key for key, value in option_set.items() if len(value) > 1]
+    # Iterate through all of the different options
+    for cfg_update in dict_product(option_set):
+        # If one of the keys in the update is a dictionary, then we need to wrap
+        # it in a list, otherwise the update will collapse the dictionary.
+        for key in cfg_update:
+            if isinstance(cfg_update[key], dict):
+                cfg_update[key] = [cfg_update[key]]
+        # Get the name that will be used for WANDB tracking and update the base with
+        # this version of the experiment.
+        cfg_name_args = proc_cfg_name(exp_name, varying_keys, cfg_update)
+        cfg = base_cfg.update([cfg_update, cfg_name_args])
+        # Verify it's a valid config
+        check_missing(cfg)
+        cfgs.append(cfg)
+    return cfgs
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def generate_config_uuids(config_list: List[Config]):
+    processed_cfgs = []
+    for config in config_list:
+        if isinstance(config, HDict):
+            config = config.to_dict()
+        create_time, nonce = generate_tuid()
+        digest = config_digest(config)
+        config['log']['uuid'] = f"{create_time}-{nonce}-{digest}"
+        # Append the updated config to the processed list.
+        processed_cfgs.append(Config(config))
+    return processed_cfgs
+
+
+def gather_exp_paths(root):
+    # For ensembles, define the root dir.
+    run_names = os.listdir(root)
+    # NOTE: Not the best way to do this, but we need to skip over some files/directories.
+    skip_items = [
+        "submitit",
+        "wandb",
+        "base.yml",
+        "experiment.yml"
+    ]
+    # Filter out the skip_items
+    valid_exp_paths = []
+    for run_name in run_names:
+        run_dir = f"{root}/{run_name}"
+        # Make sure we don't include the skip items and that we actually have valid checkpoints.
+        if (run_name not in skip_items) and os.path.isdir(f"{run_dir}/checkpoints"):
+            valid_exp_paths.append(run_dir)
+    # Return the valid experiment paths.
+    return valid_exp_paths
+
+
+def get_range_from_str(val):
+    trimmed_range = val[1:-1] # Remove the parantheses on the ends.
+    range_args = trimmed_range.split(',')
+    assert len(range_args) == 4, f"Range sweeping requires format like (start, ..., end, interval). Got {len(range_args)}."
+    arg_vals = np.arange(float(range_args[0]), float(range_args[2]), float(range_args[3]))
+    # Finally stick this back in as a string tuple version.
+    return str(tuple(arg_vals))
+
+
+def get_inference_dset_info(
+    cfg,
+    code_root
+):
+    # Total model config
+    base_model_cfg = yaml.safe_load(open(f"{cfg['experiment.model_dir']}/config.yml", "r"))
+
+    # Get the data config from the model config.
+    base_data_cfg = base_model_cfg["data"]
+    # We need to remove a few keys that are not needed for inference.
+    drop_keys = [
+        "iters_per_epoch",
+        "train_kwargs",
+        "val_kwargs",
+    ]
+    for d_key in drop_keys:
+        if d_key in base_data_cfg:
+            base_data_cfg.pop(d_key)
+
+    # Get the dataset name, and load the base inference dataset config for that.
+    base_dset_cls = base_data_cfg['_class']
+    inf_dset_cls = cfg['inference_data._class']
+
+    inf_dset_name = inf_dset_cls.split('.')[-1]
+    # Add the dataset specific details.
+    inf_dset_cfg_file = code_root / "sebench" / "configs" / "inference" / f"{inf_dset_name}.yaml"
+    if inf_dset_cfg_file.exists():
+        with open(inf_dset_cfg_file, 'r') as d_file:
+            inf_cfg_presets = yaml.safe_load(d_file)
+    else:
+        inf_cfg_presets = {}
+    # Assert that 'version' is not defined in the base_inf_dataset_cfg, this is not allowed behavior.
+    assert 'version' not in inf_cfg_presets.get("inference_data", {}), "Version should not be defined in the base inference dataset config."
+
+    # NOW WE MODIFY THE ORIGINAL BASE DATA CFG TO INCLUDE THE INFERENCE DATASET CONFIG.
+
+    # We need to modify the inference dataset config to include the data_cfg.
+    inf_dset_presets = inf_cfg_presets.get("inference_data", {})
+
+    # Now we update the trained model config with the inference dataset config.
+    new_inf_dset_cfg = base_data_cfg.copy()
+    new_inf_dset_cfg.update(inf_dset_presets)
+    # And we put the updated data_cfg back into the inf_cfg_dict.
+    inf_cfg_presets["inference_data"] = new_inf_dset_cfg
+
+    # Return the data_cfg and the base_inf_dataset_cfg
+    return inf_cfg_presets
